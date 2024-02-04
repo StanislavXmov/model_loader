3a919f00af9332c2a5524a4dc07fbaedaebc11a9
stanislav <xiiiovart@gmail.com>
2024-02-04 14:46:51 +0300

    remove grid from preview

--- client/src/components/View.tsx
+++ client/src/components/View.tsx
@@ -1,8 +1,9 @@
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { GLTF } from 'three/examples/jsm/Addons.js';
 import { useModelStore } from '../store/useModelStore';
+import { GridHelper } from 'three';
 
 interface Model3d extends GLTF {
   nodes: any;
@@ -16,24 +17,39 @@ const View = ({url}: {url: string}) => {
   const isPreview = useModelStore(s => s.isPreview);
   const name = useModelStore(s => s.name);
 
+  const gridRef = useRef<GridHelper>(null);
+
   const gl = useThree((state) => state.gl);
   const {nodes, materials} = useGLTF(url) as unknown as Model3d;
   
-  const setScreenshot = () => {
-    gl.domElement.toBlob(blob => {
-      if (blob) {
-        const file = new File([blob], `${name}_preview.png`);
-        setIsPreview(false);
-        setPreview(file);
+  const setScreenshot = async () => {
+    if (gridRef.current) {
+      gridRef.current.visible = false;
 
-        // test preview
-        // const path = URL.createObjectURL(file);
-        // const link = document.createElement('a');
-        // link.setAttribute('download', `${name}_preview.png`);
-        // link.setAttribute('href', path);
-        // link.click();
-      }
-    }, 'image/png');
+      // await next render
+      await new Promise((res) => setTimeout(() => {
+        res(null)
+      }, 500));
+
+      gl.domElement.toBlob(blob => {
+        if (blob) {
+          const file = new File([blob], `${name}_preview.png`);
+          setIsPreview(false);
+          setPreview(file);
+  
+          // test preview
+          // const path = URL.createObjectURL(file);
+          // const link = document.createElement('a');
+          // link.setAttribute('download', `${name}_preview.png`);
+          // link.setAttribute('href', path);
+          // link.click();
+
+          if (gridRef.current) {
+            gridRef.current.visible = true;
+          }
+        }
+      }, 'image/png');
+    }
   }
 
   useEffect(() => {
@@ -45,7 +61,7 @@ const View = ({url}: {url: string}) => {
   return (
     <>
       <pointLight position={[10, 10, 10]} />
-      <gridHelper />
+      <gridHelper ref={gridRef} />
       <group>
         <mesh 
           castShadow 
