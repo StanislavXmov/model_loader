f69571aea07ba327d584ee647d1b5024a105d403
stanislav <xiiiovart@gmail.com>
2024-02-06 12:08:23 +0300

    save json submited models

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,26 @@
 import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
+import fsPromises from 'fs/promises';
 import path from 'path';
 
 const app = express();
-
 const catalog = '../../static/models';
 
+type ModelProps = {
+  model: string;
+  preview: string;
+}
+
+let data: ModelProps[] = [];
+
+fsPromises.readFile(`${catalog}/data.json`, {encoding: 'utf8'})
+  .then((res) => {
+    const storeData = JSON.parse(res) as ModelProps[];
+    data = storeData;
+  });
+
 const upload = multer({dest: catalog});
-// const type = upload.array('filedata', 2);
 const type = upload.fields([{name: 'model', maxCount: 1}, {name: 'preview', maxCount: 1}]);
 
 app.use(express.static('../client/dist/'));
@@ -19,8 +31,7 @@ app.get('/', (_, res) => {
 });
 
 app.post('/upload', type, (req, res) => {
-	// console.log('files', req.files);
-  // const files = req.files as Express.Multer.File;
+	
   const files = req.files as unknown as { [fieldname: string]: Express.Multer.File[]; };
   const name = req.body.name as string;
 
@@ -29,8 +40,11 @@ app.post('/upload', type, (req, res) => {
   const model = files.model[0];
   const preview = files.preview[0];
 
+  const modelName = `${name}.${model.originalname.split('.')[1]}`;
+  const previewName = preview.originalname;
+  
   const tmpModelPath = model.path;
-  const targetModelPath = path.join(model.destination, `${name}.${model.originalname.split('.')[1]}`);
+  const targetModelPath = path.join(model.destination, modelName);
   const srcModel = fs.createReadStream(tmpModelPath);
   const destModel = fs.createWriteStream(targetModelPath);
 
@@ -38,7 +52,7 @@ app.post('/upload', type, (req, res) => {
   srcArr.push({src: srcModel, tmpPath: tmpModelPath});
 
   const tmpPreviewPath = preview.path;
-  const targetPreviewPath = path.join(preview.destination, preview.originalname);
+  const targetPreviewPath = path.join(preview.destination, previewName);
   const srcPreview = fs.createReadStream(tmpPreviewPath);
   const destPreview = fs.createWriteStream(targetPreviewPath);
 
@@ -53,6 +67,15 @@ app.post('/upload', type, (req, res) => {
     });
   }
 
+  data.push({
+    model: modelName,
+    preview: previewName,
+  });
+
+  fsPromises.writeFile(`${catalog}/data.json`, JSON.stringify(data, null, 2))
+    .then(() => console.log('Submited model'))
+    .catch(() => console.log('Failed to write updated data to file'));
+  
 	res.end();  
 });
 
